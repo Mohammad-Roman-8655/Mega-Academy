06bb290be7ffa9ee9d28933d337efd675c7c00bd
Mohammad Roman <mohammadroman8655@gmail.com>
2025-03-04 15:26:40 +0530

    Nothing changes

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-        '/api/v1': {
-            target: 'http://localhost:8080',
-            secure: false,
-        },
-    },
+    // proxy: {
+    //     '/api/v1': {
+    //         target: 'http://localhost:8080',
+    //         secure: false,
+    //     },
+    // },
 },
   plugins: [react()],
 })
